ffb7679a13b525fe3d50fc6ecd1a52a72cba12a6
Ryx076 <Ryx076@users.noreply.github.com>
2024-06-25 19:59:30 +0000

    deploy: 4086e3d8f17ee2ab55d73332140e9e07cb0ebe7f

---
+++ 10/assets/js/6302f74b.974b6dcf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkessentials_security=self.webpackChunkessentials_security||[]).push([[7667],{4034:e=>{e.exports=JSON.parse('{"title":"Scratch Linux","slug":"/Scratch Linux/","permalink":"/essentials-security/10/Scratch Linux/","navigation":{"next":{"title":"Welcome to Linux","permalink":"/essentials-security/10/Scratch Linux/Welcome to Linux/"}}}')}}]);
