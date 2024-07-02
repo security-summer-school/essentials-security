2efdafdaa72320e920c09e8e2bef319d54c7d41f
teodutu <teodutu@users.noreply.github.com>
2024-07-02 11:02:27 +0000

    deploy: bcc9241116d68822edb24e3180d3e7c047ee55b4

---
+++ 11/assets/js/3cdcb5b5.094b35f3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkessentials_security=self.webpackChunkessentials_security||[]).push([[1122],{3253:e=>{e.exports=JSON.parse('{"title":"Scratch Linux","slug":"/Scratch Linux/","permalink":"/essentials-security/11/Scratch Linux/","navigation":{"next":{"title":"Welcome to Linux","permalink":"/essentials-security/11/Scratch Linux/Welcome to Linux/"}}}')}}]);
