3b6fe48fbabc869fa83cd8eff419baeef12c6eff
AdoX13 <AdoX13@users.noreply.github.com>
2024-07-08 13:21:10 +0000

    deploy: 396edd2e7dea3c2a45145dc1f4e957f97981148b

---
+++ 13/assets/js/96dc7a9c.4a0c4345.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkessentials_security=self.webpackChunkessentials_security||[]).push([[5213],{9996:e=>{e.exports=JSON.parse('{"title":"Scratch Linux","slug":"/Scratch Linux/","permalink":"/essentials-security/13/Scratch Linux/","navigation":{"next":{"title":"Welcome to Linux","permalink":"/essentials-security/13/Scratch Linux/Welcome to Linux/"}}}')}}]);
